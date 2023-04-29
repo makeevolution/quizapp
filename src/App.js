fa0e2f3aa14cba6908e1afa9050a63b5eef092c8
makeevolution <aldo_hasibuan@yahoo.com>
2023-04-29 18:28:10 +0200

    add 3 routes for quiz

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, 
+  Routes, Route} from "react-router-dom"
+import Settings from './pages/Settings'
+import Questions from "./pages/Questions"
+import FinalScreen from "./pages/FinalScreen"
 
 function App() {
+
+  // Helper Functions
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Settings />} />
+        <Route path="/questions" element={<Questions />} />
+        <Route path="/finalscreen" element={<FinalScreen />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/FinalScreen.js
@@ -0,0 +1,8 @@
+
+const FinalScreen = () => {
+  return (
+    <div><h1>Final Screen</h1></div>
+  );
+}
+
+export default FinalScreen
