f1c080cbd3bdb82980c4732c30fce9740af417ee
makeevolution <aldo_hasibuan@yahoo.com>
2023-04-29 21:41:04 +0200

    add no of questions desired to be quizzed on

---
+++ src/components/TextFieldComp.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { FormControl, TextField } from '@mui/material'
+import { Box } from '@mui/system'
+
+const TextFieldComp = () => {
+  const handleChange = () => {};
+
+  return (
+    <Box mt={3} width="100%">
+        <FormControl fullWidth size="small">
+            <TextField
+                onChange={handleChange}
+                variant="outlined"
+                label="Amount of Questions"
+                type="number"
+                size="small"
+                />
+        </FormControl>
+    </Box>
+  )}
+
+export default TextFieldComp
--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,13 +1,19 @@
 import SelectField from "../components/SelectField"
+import TextFieldComp from "../components/TextFieldComp"
 import { Button } from "@mui/material"
 import { Box } from "@mui/system";
 
 const Settings = () => {
+
+  const handleSubmit = e => {
+    e.preventDefault();  // Do not let the browser refresh on post
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <SelectField label="Category" />
       <SelectField label="Difficulty" />
       <SelectField label="Type" />
+      <TextFieldComp label="No of questions" />
       <Box mt={3} width="100%">
         <Button fullWidth variant="contained" type="submit">
           Get started
